ecd889104bf478ccd90182fba8bed34e423aa42e
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 12:16:17 -0600

    Create Practica_034_Funciones.py

---
+++ Practica_034_Funciones.py
@@ -0,0 +1,14 @@
+#para crear funciones utilizamos def nombrefuncion()
+#todo lo que esta dentro de la funcion se ejecuta una vez llamemos a la funcion
+
+#lor argumentos son variables que se pueden especificar por defecto al llamar una funcion
+#son declarados directamente sobre los parenesis de la funcion
+#seran obligatorios cada vez que llames a la funcion
+
+#se definen dos argumentos
+def suma(numero1, numero2):
+	print(numero1 + numero2) #se suman si son int
+
+suma(12, 18) #se dan los argumentos cada vez que se llama
+suma(25, 25)
+suma(50000, 7000)
