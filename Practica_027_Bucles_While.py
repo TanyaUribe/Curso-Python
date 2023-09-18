948e88025985e21a7607213be1ff4f8c1e52922c
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 09:02:34 -0600

    Create Practica_027_Bucles_While.py

---
+++ Practica_027_Bucles_While.py
@@ -0,0 +1,21 @@
+#el bucle while
+#ejecuta una serie de declaraciones eiempre que la condicion se cumpla (TRUE)
+#cuando es falase se deja de ejecutar el codigo
+
+x = 0
+while(x <= 15):
+        print(x)
+        x += 5
+#operadores
+        #+= incremento
+        #-= decremento
+y = 0
+while(y >= -100):
+        print(y)
+        y -= 20
+
+z = 10
+while(z >= 0):
+        print("El valor del bucle es:", z)
+        z -= 1
+        
