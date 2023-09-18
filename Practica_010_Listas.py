1c5d629adb0b858a3473b080cc84b737bc5d106d
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 23:36:04 -0600

    Create Practica_010_Listas.py
    Basicos de las listas

---
+++ Practica_010_Listas.py
@@ -0,0 +1,15 @@
+#las listas en python son como los arrays en otros lenguajes de programacion
+#pueden contener cualquier tipo de dato, en cualquier orden
+#utilizan corchetes para definirse
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+
+
+#las posiciones de cada elemento comienzan desde el 0
+#el elemento en la posición 0 sería "rojo"
+#"rosa" sería el elemento 7
+
+print(colores[0]) #para imprimir solo un elemento se especifica su posición dentro del corchete
+print(colores[7])
+
+numeros = ["tres","dos","cinco","cuatro","uno"]
+print(numeros) #si imprimimos la lista de esta manera, se imprime tal cual la vemos en código
