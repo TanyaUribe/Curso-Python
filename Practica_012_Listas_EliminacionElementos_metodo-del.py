f9c206c022b1097faac8c154dec60f742d5d6744
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 23:47:01 -0600

    Create Practica_012_Listas_EliminacionElementos_metodo-del.py
    Método del para eliminacion de elementos en una lista

---
+++ Practica_012_Listas_EliminacionElementos_metodo-del.py
@@ -0,0 +1,14 @@
+#el metodo del()
+#utiliza el numero de posición del elemento a liminar
+#se pueden utilizar posiciones negativas
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#se escribe primero el nombre del metodo, despues la lista y
+#el numero de posición dentro de corchetes
+del colores[1]
+del colores[3]
+del colores[4]
+del colores[-3]
+
+print(colores)
