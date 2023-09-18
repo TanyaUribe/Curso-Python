84f23bab5180746e7568534447a686980ba7ef08
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 22:25:21 -0600

    Create Practica_002_Variables.py
    Cómo se utilizan y definen las variables en Python

---
+++ Practica_002_Variables.py
@@ -0,0 +1,14 @@
+#se crea una variable con un mensaje
+#no es necesario ninguna palabra reservada para definir una variable
+
+texto = "aguacate"
+#si nuestra variable es string el valor se pone entre comillas
+a = 4
+b = 3
+
+c = (a+b)
+#la suma de las dos variables previamente definidas se define en otra variable
+
+#imprimimos el valor resultante
+print(c); print(texto)
+#el uso de ; no es necesario a menos que pongamos múltiples instrucciones en una línea
