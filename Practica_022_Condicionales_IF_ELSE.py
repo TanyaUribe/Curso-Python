cb301d72074cded0b9e47f68a1d6b60780b92c42
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 07:43:51 -0600

    Create Practica_022_Condicionales_IF_ELSE.py
    Condicional elif

---
+++ Practica_022_Condicionales_IF_ELSE.py
@@ -0,0 +1,10 @@
+#el if es para cuando la condición sea TRUE
+#el else es para cuando sea FALSE
+
+color = "rojo"
+
+if (color == "rojo"): #lleva dos puntos
+    print("El color es rojo.")
+
+else: #lleva dos puntos, no lleva condicion. El else esta sujeto a su condicion if principal
+    print("El color no es rojo.")
