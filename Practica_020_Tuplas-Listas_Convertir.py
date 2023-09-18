e3b26e1c6201d0d968f11ab589d4c6e30cc30ad7
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 07:14:51 -0600

    Create Practica_020_Tuplas-Listas_Convertir.py
    Cómo convertir una tupla a lista o viceversa

---
+++ Practica_020_Tuplas-Listas_Convertir.py
@@ -0,0 +1,9 @@
+#el metodo tuple crea una tupla a partir de una lista
+#el metodo list crea una lista a partir de una tupla
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+tupla = tuple(colores)
+
+#el metodo type() nos dice que tipo de dato es algo
+print(type(tupla))
+print(tupla)
