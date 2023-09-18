8e360ba26b146e57f62d4d0eb2d9cc3b40f8b538
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 22:31:26 -0600

    Create Practica_004_Concatenacion_Variables.py
    Cómo concatenar (unir) dos strings, y cómo concatenar dos variables con números

---
+++ Practica_004_Concatenacion_Variables.py
@@ -0,0 +1,25 @@
+#para concatenar, se utiliza el símbolo +
+string1= "Hola! "
+string2 = " Buen día"
+res= string1 + string2
+print(res)
+
+a = "Tanya"
+b = "Uribe"
+#podemos concatenar un string con espacio sin haberlo almacenado en una variable
+c = a + " " + b
+print(c)
+
+#para concatenar números, éstos deben ponerse en comillas
+n1 = "1"
+n2 = "2"
+n3 = n1 + n2
+print(n3)
+
+#para poder mostrar los caractéres con acento se usa lo siguiente
+#al inicio de cada programa (se quedan como comentarios)
+
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
