0f712908aae439b993757e3a0e4b01e5149fc5c4
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 22:32:24 -0600

    Create Practica_005_Strings_MétodosUpperLowerTitle.py
    Uso de los métodos upper(), lower() y title() en strings.

---
+++ Practica_005_Strings_MétodosUpperLowerTitle.py
@@ -0,0 +1,19 @@
+#el método title es para poner cada primer letra de cada palabra
+#en un string en mayúscula
+
+nombrep = "enrrique barros fernandez"
+nombrep =nombrep.title()
+print(nombrep)
+
+#el método lower cambia todas las letras a minúsculas
+#otra manera de escribir la conversion con el método
+f1 = "Esta Es Una Frase Para Ser Formateada.".lower()
+print(f1)
+
+#el método upper se utiliza para poner todas las letras
+#de un string en mayúsculas
+#otra manera de escribir la conversion con el método
+print(f1.upper())
+
+
+
