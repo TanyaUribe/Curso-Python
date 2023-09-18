e6577d375ce3e61bebf66c4369c014fb2c565f55
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 00:03:53 -0600

    Create Practica_013_Listas_EliminacionElementos_metodo-remove.py
    Metodo de eliminacion de un elemento de una lista mediante el metodo remove()

---
+++ Practica_013_Listas_EliminacionElementos_metodo-remove.py
@@ -0,0 +1,10 @@
+#el metodo remove()
+#utiliza el nombre del elemento a eliminar
+#dentro de paréntesis
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#se escribe el nombre de la lista, el método remove y dentro de los parentesis el nombre del elemento a eliminar
+colores.remove("amarillo")
+colores.remove("rojo")
+print(colores)
