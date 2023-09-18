4e23c8425a885f902c1d74d54b109f5d2d94f5aa
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 00:04:31 -0600

    Create Practica_015_Listas_InsertarElementos_metodo-append.py
    Metodo de adición de un elemento de una lista mediante el metodo apppend()

---
+++ Practica_015_Listas_InsertarElementos_metodo-append.py
@@ -0,0 +1,9 @@
+#el metodo append()
+#añade un elemento al FINAL de la lista
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#se escribe el nombre de la lista, el método append y entre parentesis el nombre del elemento a añadir
+colores.append("fuxia")
+colores.append("celeste")
+print(colores)
