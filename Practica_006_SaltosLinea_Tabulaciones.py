e052f9bd377a963320a2a42a02377fa073b88431
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 22:42:19 -0600

    Create Practica_006_SaltosLinea_Tabulaciones.py
    Como hacer tabulaciones y saltos de linea

---
+++ Practica_006_SaltosLinea_Tabulaciones.py
@@ -0,0 +1,7 @@
+#para hacer un salto de línea se utiliza \n
+#para hacer tabulaciones \t
+
+leng = "-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-C++\n-C#"
+print(leng)
+
+#se pueden añadir tantas como sean necesarias
