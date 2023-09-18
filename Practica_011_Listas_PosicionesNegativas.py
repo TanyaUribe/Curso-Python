01b4af0db77c2d9764400d67f330596b88805574
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 23:37:43 -0600

    Create Practica_011_Listas_PosicionesNegativas.py
    Uso de posiciones negativas en listas en Python

---
+++ Practica_011_Listas_PosicionesNegativas.py
@@ -0,0 +1,14 @@
+#las posiciones negativas en las listas se utilizan para
+#acceder mas facilmente a los ultimos elementos de una lista
+
+#las posiciones negativas comienzan a contar desde el ultimo elemento
+#hasta el primero
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#la posición -1 sería la última posición, la -2 la penúltima y así sucesivamente
+print(colores[-1])
+print(colores[-7])
+print(colores[-5])
+print(colores[-2])
+print(colores[-10])
