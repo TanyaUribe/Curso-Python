e25139578b397139caeabc91d9e337e9c3df3382
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 22:28:40 -0600

    Create Practica_003_Strings.py
    Uso/definicion de Strings. Cómo utilizar las comillas dobles y simples según se necesite.

---
+++ Practica_003_Strings.py
@@ -0,0 +1,13 @@
+#se crean dos variables string
+s1 = 'a'
+s2 = "a"
+#se pueden utilizar comillas dobles o simples
+
+s1 = '"print()" se utiliza para imprimir valores en la consola.'
+#si se necesita poner comillas dobles dentro del string,
+#se escribe en comillas simples
+#si se requieren comillas simples dentro, el string se escribe dentro de comillas dobles
+
+s2 = "'print()' se utiliza para imprimir valores en la consola."
+print(s1)
+print(s2)
