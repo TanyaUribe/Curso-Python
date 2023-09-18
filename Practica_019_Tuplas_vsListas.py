7959f662399aaa10a7d5d30fefc260adcf242800
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 07:14:36 -0600

    Create Practica_019_Tuplas_vsListas.py
    Definicion de tuplas en comparacion con las listas

---
+++ Practica_019_Tuplas_vsListas.py
@@ -0,0 +1,16 @@
+#las tuplas son inmutables, no pueden modificarse
+#las listas son mutables
+
+#al ser definidas, las tuplas usan parentesis
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+#para llamar a un elemento se usan corchetes
+print(colores[1])
+
+#las tuplas como las listas soportan  cualquier tipo de dato
+numeros = (10, 1, 5, 11)
+
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+print(operacion)
+
+
