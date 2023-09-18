5f1965575d3e4c6db5ff7e4425813e47250b5e01
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 23:35:51 -0600

    Create Practica_009_Datos_FloatyMetodoRound.py
    Tipo de dato float, y metodo round() para este tipo de dato

---
+++ Practica_009_Datos_FloatyMetodoRound.py
@@ -0,0 +1,8 @@
+#para los datos tipo float se utiliza el punto decimal
+a = 0.0
+a = 17.567383292929200234
+
+#el método round sirve para redondear números
+#podemos especificar el número de decimales requeridos en el segundo parametro
+print(round(a,5))
+#aqui se especifican 5 decimales para la variable a
