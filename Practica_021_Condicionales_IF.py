1fd59f9e91cffb7dcddadc34b9147a5e51c7f0e3
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-18 07:15:08 -0600

    Create Practica_021_Condicionales_IF.py
    Condicional if y como se utiliza

---
+++ Practica_021_Condicionales_IF.py
@@ -0,0 +1,26 @@
+#El condicional if se ejecuta si la condicion es TRUE
+#si es FALSE se salta el codigo dentro del if
+
+#las condiciones que podemos escribir en los ifs son booleanas (true o false)
+#operadores: <, >, <= menor o igual, >= mayor o igual, == igual que, != diferente
+
+num1 = 15
+num2 = 20
+
+if num1 < num2: 
+	print('Se ejecuta el if.')
+        #la condicion es TRUE
+	
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if.')
+        #la condición es TRUE
+
+num1 = 1450
+num2 = 60
+
+if num1 == num2:
+	print('Se ejecuta el if.')
+        #la condición es FALSE, no se imprime
