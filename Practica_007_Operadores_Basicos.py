af6ae9516d905b68212132520bbf7e8cd96b1e82
Tanya Uribe <126315885+flyingtotopo@users.noreply.github.com>
2023-09-17 23:35:07 -0600

    Create Practica_007_Operadores_Basicos.py
    Operadores básicos

---
+++ Practica_007_Operadores_Basicos.py
@@ -0,0 +1,11 @@
+#+ suma, - resta, / división, * multiplicacion
+a = 20+23+4+40
+b = 20-23-4-30-50
+c = 10*87
+d = 5000/230/2
+#tambien se pueden utilizar parentesis como normalmente
+#para dar prioridad sobre ciertas operaciones
+operacion = (10/5)+15-17
+
+#para concatenar valores numericos se utilizan comas
+print(a,b,c,d,operacion)
